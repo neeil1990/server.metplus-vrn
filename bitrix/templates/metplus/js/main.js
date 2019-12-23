21abed142360a89f17bbb01ecda8bd29cf46fb3a
neeil1990 <neeil@mail.ru>
2019-12-23 15:19:04 +0300

    no message

--- bitrix/templates/metplus/js/main.js
+++ bitrix/templates/metplus/js/main.js
@@ -239,8 +239,9 @@ jQuery(document).ready(function($) {
   });
   $(".wrapper_select-office").on("change", function() {
     var number = $(this).find('option:selected').index();
+    
     $(this).closest('.tab-item').find('.contact-section-desc').removeClass('is-active').eq(number).addClass('is-active');
-    $(this).closest('.container').find('.contact_right-column .contact-img.is-visible .contact-section-desc').removeClass('is-active').eq(number).addClass('is-active');
+    $(this).closest('.container').find('.contact_right-column .contact-img.is-visible img.contact-section-desc').removeClass('is-active').eq(number).addClass('is-active');
   });
 
   $(".map-contact_box .js-select").on("change", function() {
