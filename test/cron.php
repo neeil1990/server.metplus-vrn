c53a3a17422c7c0307bf9b8ee85617f547648d59
neeil1990 <neeil@mail.ru>
2019-12-26 10:42:24 +0300

    no message

--- test/cron.php
+++ test/cron.php
@@ -1,9 +1,5 @@
 <?
-//echo str_pad("Last Execution Time: " . date('l jS \of F Y h:i:s A'), 100);
-$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'] = realpath(dirname(__FILE__) . '/../');
 
-define('NO_KEEP_STATISTIC', true);
-define('NOT_CHECK_PERMISSIONS',true);
 
 require($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
 
