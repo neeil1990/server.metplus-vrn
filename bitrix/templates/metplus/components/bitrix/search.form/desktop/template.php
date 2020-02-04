bd2e2393f5186f0c27960a8081bfa0b33866b2e7
neeil1990 <neeil@mail.ru>
2020-02-04 16:04:44 +0300

    search

--- bitrix/templates/metplus/components/bitrix/search.form/desktop/template.php
+++ bitrix/templates/metplus/components/bitrix/search.form/desktop/template.php
@@ -10,9 +10,10 @@
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
-$this->setFrameMode(true);?>
+$this->setFrameMode(true);
+?>
 <form action="<?=$arResult["FORM_ACTION"]?>" class="head_form-search mobile-hidden">
-    <input type="text" name="q" class="head_form-search_input" placeholder="Введите ваш запрос">
+    <input type="text" name="q" class="head_form-search_input" value="<?=$_REQUEST['q']?>" placeholder="Введите ваш запрос">
     <div class="wrapper_submit-btn">
         <span class="glipf-search"></span>
         <input type="submit" name="s" class="form-search_submit-btn" value="">
